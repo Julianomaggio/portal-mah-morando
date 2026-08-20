5e276c185f3dd44a26ccc311c5dcad01fc5c195f
Julianomaggio <j.maggioc7@gmail.com>
2026-08-20 12:15:51 -0300

    Adiciona configuração central preparada para API e autenticação

---
+++ app-config.js
@@ -0,0 +1,14 @@
+window.PORTAL_CONFIG = {
+  environment: 'production',
+  apiBaseUrl: '',
+  newsletterEndpoint: '',
+  authProvider: '',
+  features: {
+    search: true,
+    newsletter: true,
+    columnistArea: true,
+    adminArea: true,
+    advertising: true,
+    video: true
+  }
+};
